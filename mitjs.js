47f3254e03f0d011f1f0367cd69c8e80991db91c
Silke Marie  Steen Olesen <silkemariesteenolesen@Silkes-MacBook-Air.local>
2019-03-28 13:50:30 +0100

    Update mitjs.js

--- mitjs.js
+++ mitjs.js
@@ -1,17 +1,14 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
 }
